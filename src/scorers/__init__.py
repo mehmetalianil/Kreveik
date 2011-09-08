553df74b39de3102be9775fdaaa45fe4268eb222
Mehmet Ali Anil <mehmet.ali.anil@gmail.com>
2011-09-08 21:23:25 +0300

    The sum_scorer is normalized to 1, thus all scores are less than 1 from now on.

--- src/scorers/__init__.py
+++ src/scorers/__init__.py
@@ -1,2 +1,4 @@
-def sum_scorer(family):
-    return sum(family.equilibria)
+def sum_scorer(network):
+    # The worst case is an orbit that walks the whole state space.
+    # Thus the scores are normalized to that value.
+    return sum(network.equilibria)/(2.0**(2*network.n_nodes))
