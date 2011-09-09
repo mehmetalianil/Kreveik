9d5873e636cd4f35a4e6026786f7397ea37f4f51
Mehmet Ali Anil <mehmet.ali.anil@gmail.com>
2011-09-09 05:26:15 +0300

    General Restructuring

---
+++ src/genereg/__init__.py
@@ -0,0 +1,26 @@
+'''
+Created on 09.02.2011
+
+@author: Mehmet Ali Anil
+
+The genereg package maintains class definitions and methods for manipulation,
+statistical evaluation of random boolean networks. The emphasis is upon genetic
+algorithm and how networks evolve accordingly.
+'''
+
+print_enable = True
+
+
+__author__ = "Mehmet Ali Anil"
+__copyright__ = ""
+__credits__ = ["Mehmet Ali Anil"]
+__license__ = ""
+__version__ = "0.0.5"
+__maintainer__ = "Mehmet Ali Anil"
+__email__ = "anilm@itu.edu.tr"
+__status__ = "Production"
+
+from family import *
+from generators import *
+from network import *
+
---
+++ src/genereg/generators.py
@@ -0,0 +1,33 @@
+import numpy as num
+from family import *
+from network import *
+print_enable=True
+
+def generate_random(n_nodes,scorer,function,probability=(0.5,0.5,0.5)):
+    '''
+    Generates and returns a random network with a random initial conditions.
+    The adjacency matrix, initial state, boolean function are populated with 
+    1's and 0's with probabilities of 0.5.
+    
+    Input Variables
+    n_nodes -> Number of nodes in the system. A value of 2 to 8 will generate a 
+    network with manageable size and complexity.
+    probability -> The probability of having a connection in any two nodes. 
+    A smaller value will decrease complexity and clustering coefficient.  
+    scorer -> a function for the type network
+    '''
+    num.random.seed()
+    adjacency_matrix=(num.random.random((n_nodes,n_nodes))<probability[0])*1.0
+    #    First state of the system is determined
+    state=(num.random.random((1,n_nodes))<probability[1])*1.0
+    bool_fcn=(num.random.random((n_nodes,n_nodes))<probability[2])*1.0
+
+    try:
+        return network(adjacency_matrix, bool_fcn, scorer,function,state_vec=state)
+        
+    except ValueError,e:
+        z = e
+        print "Network is too big to model."
+        print z
+    
+    return 
