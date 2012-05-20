70d30512b395f9e32ecc7a10e8f6e8739589c140
Mehmet Ali Anil <mehmet.ali.anil@gmail.com>
2012-05-20 22:04:48 +0300

    Added Apache 2.0 bilerplate to all source files. Licensed with Apache License 2.0 , added LICENSE and NOTICE files.

--- kreveik/network/boolfuncs/boolfuncs.py
+++ kreveik/network/boolfuncs/boolfuncs.py
@@ -1,15 +1,22 @@
-'''
-Created on Sep 8, 2011
+
+#    Copyright 2012 Mehmet Ali ANIL
+#    
+#    Licensed under the Apache License, Version 2.0 (the "License");
+#    you may not use this file except in compliance with the License.
+#    You may obtain a copy of the License at
+#    
+#    http://www.apache.org/licenses/LICENSE-2.0
+#    
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS,
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    See the License for the specific language governing permissions and
+#    limitations under the License.
+
 '''
 
-__author__ = "Mehmet Ali Anil"
-__copyright__ = ""
-__credits__ = ["Mehmet Ali Anil"]
-__license__ = ""
-__version__ = "0.0.5"
-__maintainer__ = "Mehmet Ali Anil"
-__email__ = "anilm@itu.edu.tr"
-__status__ = "Production"
+'''
+
         
 import numpy as num
 import logging
