ab044880eb149ad42cdc7bfc1f2245479ef32b67
Mehmet Ali Anil <mehmet.ali.anil@gmail.com>
2012-05-21 07:50:49 +0300

    Worked on docstrings of network.generators.

--- kreveik/network/generators.py
+++ kreveik/network/generators.py
@@ -20,32 +20,52 @@ def create(adjacency_matrix, bool_fcn, function):
     Generates and returns a network with a given adjacency matrix. The initial state 
     is populated with 0's. 
     
-    Input Variables
-    adjacency_matrix -> The matrix composed of adjacency values between the nodes.
-    bool_fcn -> Mask function
+    Input Variables:
+    ----------------
+    
+    adjacency_matrix
+        The matrix composed of adjacency values between the nodes.
+    bool_fcn
+        Mask function
     """
+    import numpy as num
+    
     initial_state= num.array([len(adjacency_matrix)*[False]])
     new_network=Network(adjacency_matrix, bool_fcn, function, state_vec = initial_state)
     return new_network
     
-def random(n_nodes,function,probability=(0.5,0.5,0.5),connected=False):
-    '''
+def random(n_nodes,function,probability=(0.5,0.5,0.5),connected=False, howmany=1):
+    """
     Generates and returns a random network with a random initial conditions.
     The adjacency matrix, initial state, boolean function are populated with 
     1's and 0's with probabilities of 0.5.
     
-    Input Variables
-    n_nodes -> Number of nodes in the system. A value of 2 to 8 will generate a 
-    network with manageable size and complexity.
-    probability -> The probability of having a connection in any two nodes. 
-    A smaller value will decrease complexity and clustering coefficient.  
-    scorer -> a function for the type network
-    '''
+    Input Variables:
+    ---------------
+    
+    n_nodes        
+        Number of nodes in the system.
+    function
+        A function which maps the state of a network to another. 
+    probability (a,b,c)
+        (a) The probability of having a connection in any two nodes 
+            Defaults to 0.5 
+        (b) The probability of having an initial state of a node True.
+            Defaults to 0.5
+        (c) The probability of having a Boolean function key element True.
+            Defaults to 0.5
+    connected
+        if True, only fully connected networks are generated. Defaults to False.
+    howmany
+        the number of networks that will be generated. Defaults to 1.
+    """
     
     import numpy as num
     import kreveik
     import logging
 
+    #TODO: howmany function
+    
     num.random.seed()
     adjacency_matrix=(num.random.random((n_nodes,n_nodes))<probability[0])
     state=(num.random.random((1,n_nodes))<probability[1])
